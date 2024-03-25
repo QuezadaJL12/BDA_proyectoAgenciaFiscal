638ab01d867dcbe27dc95c64b7e550d8d7614505
QuezadaJL12 <jose.quezada236053@potros.itson.edu.mx>
2024-03-24 17:18:59 -0700

    se agregaron los proyectos

--- AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/AgenciaFiscalPresentacion.java
+++ AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/AgenciaFiscalPresentacion.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyectoagenciafiscal;
+package com.mycompany.agenciafiscalpresentacion;
 
 /**
  *
  * @author Usuario
  */
-public class ProyectoAgenciaFiscal {
+public class AgenciaFiscalPresentacion {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
